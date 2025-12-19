3a977da89d10a7ddcb20453e1a3f3bd3e8f374c4
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2025-12-19 00:10:41 -0300

    first commit

---
+++ src/routes/gerador/admissional/+layout.svelte
@@ -0,0 +1,17 @@
+<script>
+</script>
+
+<div id="container">
+	<input type="text" placeholder="Nome" class="input" />
+</div>
+
+<slot></slot>
+
+<style>
+	#container {
+		display: flex;
+		margin: auto;
+		width: 50%;
+		justify-items: center;
+	}
+</style>
