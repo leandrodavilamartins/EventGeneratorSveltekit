97af4d7aaa9dcb02293e40559a795aad5146fc00
Leandro D'Ávila Martins Braga <leandrodavilamartins@gmail.com>
2025-12-20 21:49:57 -0300

    leiaute s2220 completo

--- src/routes/gerador/admissional/+layout.svelte
+++ src/routes/gerador/admissional/+layout.svelte
@@ -1,4 +1,162 @@
 <script>
+	function xmlS2200() {
+		let msg = `<?xml version="1.0" encoding="UTF-8"?>
+<eSocial xmlns="http://www.esocial.gov.br/schema/evt/evtAdmissao/v_S_01_03_00">
+    <evtAdmissao>
+        <ideEvento>
+            <indRetif>${indRetif}</indRetif>
+            <nrRecibo>${nrRecibo}</nrRecibo>
+            <tpAmb>${tpAmb}</tpAmb>
+            <procEmi>${procEmi}</procEmi>
+            <verProc>${verProc}</verProc>
+        </ideEvento>
+        <ideEmpregador>
+            <tpInsc>${tpInsc}</tpInsc>
+            <nrInsc>${nrInsc}</nrInsc>
+        </ideEmpregador>
+        <trabalhador>
+            <cpfTrab>${cpfTrab}</cpfTrab>
+            <nmTrab>${nmTrab}</nmTrab>
+            <sexo>${sexo}</sexo>
+            <racaCor>${racaCor}</racaCor>
+            <estCiv>${estCiv}</estCiv>
+            <grauInstr>${grauInstr}</grauInstr>
+            <nmSoc>${nmSoc}</nmSoc>
+            <nascimento>
+                <dtNascto>${dtNascto}</dtNascto>
+                <paisNascto>${paisNascto}</paisNascto>
+                <paisNac>${paisNac}</paisNac>
+            </nascimento>
+            <endereco>
+                <brasil>
+                    <tpLograd>${tpLograd}</tpLograd>
+                    <dscLograd>${dscLograd}</dscLograd>
+                    <nrLograd>${nrLograd}</nrLograd>
+                    <complemento>${complemento}</complemento>
+                    <bairro>${bairro}</bairro>
+                    <cep>${cep}</cep>
+                    <codMunic>${codMunic}</codMunic>
+                    <uf>${uf}</uf>
+                </brasil>
+                <exterior>
+                    <!-- Campos ignorados. Preencher somente em caso de trabalhador no exterior -->
+                </exterior>
+            </endereco>
+            <infoDeficiencia>
+                <!-- Campos ignorados. Preencher somente em caso de portador de deficiência -->
+            </infoDeficiencia>
+            <dependente> <!-- Preencher os campos abaixo em caso de dependente-->
+                <tpDep></tpDep>
+                <nmDep></nmDep>
+                <dtNascto></dtNascto>
+                <cpfDep></cpfDep>
+                <sexoDep></sexoDep>
+                <depIRRF></depIRRF>
+                <depSF></depSF>
+                <incTrab></incTrab>
+                <descrDep></descrDep>
+            </dependente>
+            <contato>
+                <fonePrinc>${fonePrinc}</fonePrinc>
+                <emailPrinc>${emailPrinc}</emailPrinc>
+            </contato>
+        </trabalhador>
+        <vinculo>
+            <matricula>${matricula}</matricula>
+            <tpRegTrab>${tpRegTrab}</tpRegTrab>
+            <tpRegPrev>${tpRegPrev}</tpRegPrev>
+            <cadIni>${cadIni}</cadIni>
+            <infoRegimeTrab>
+                <infoCeletista>
+                    <dtAdm>${dtAdm}</dtAdm>
+                    <tpAdmissao>${tpAdmissao}</tpAdmissao>
+                    <indAdmissao>${indAdmissao}</indAdmissao>
+                    <nrProcTrab>${nrProcTrab}</nrProcTrab>
+                    <tpRegJor>${tpRegJor}</tpRegJor>
+                    <natAtividade>${natAtividade}</natAtividade>
+                    <dtBase>${dtBase}</dtBase>
+                    <cnpjSindCategProf>${cnpjSindCategProf}</cnpjSindCategProf>
+                    <matAnotJud>${matAnotJud}</matAnotJud>
+                    <FGTS>
+                        <dtOpcFGTS>${dtOpcFGTS}</dtOpcFGTS>
+                    </FGTS>
+                    <trabTemporario> <!-- Preencher apenas se trabalho temporário -->
+                        <ideEstabVinc>${ideEstabVinc}</ideEstabVinc>
+                        <ideTrabSubstituido>${ideTrabSubstituido}</ideTrabSubstituido>
+                    </trabTemporario>
+                    <aprend></aprend> <!-- Grupo "aprendiz" . Preencher apenas se aprendiz -->
+                </infoCeletista>
+                <infoEstatutario><!-- Grupo "Trabalho estatutário"--></infoEstatutario>
+            </infoRegimeTrab>
+            <infoContrato>
+                <nmCargo>${nmCargo}</nmCargo>
+                <CBOCargo>${CBOCargo}</CBOCargo>
+                <dtIngrCargo>${dtIngCargo}</dtIngrCargo>
+                <nmFuncao>${nmFuncao}</nmFuncao>
+                <CBOFuncao>${CBOFuncao}</CBOFuncao>
+                <acumCargo>${acumCargo}</acumCargo>
+                <codCateg>${codCateg}</codCateg>
+                <remuneracao>
+                    <vrSalFx>${vrSalFx}</vrSalFx>
+                    <undSalFixo>${undSalFixo}</undSalFixo>
+                    <dscSalVar>${dscSalVar}</dscSalVar>
+                </remuneracao>
+                <duracao>
+                    <tpContr>${tpContr}</tpContr>
+                    <dtTerm>${dtTerm}</dtTerm>
+                    <clauAssec${clauAssec}></clauAssec>
+                    <objDet>${objDet}</objDet>
+                </duracao>
+                <localTrabalho>
+                    <localTrabGeral>
+                        <tpInsc>${tpInsclocalTrabalho}</tpInsc>
+                        <nrInsc>${nrInsclocalTrabalho}</nrInsc>
+                        <descComp>${descComp}</descComp>
+                    </localTrabGeral>
+                    <localTempDom></localTempDom>
+                </localTrabalho>
+                <horContratual>
+                    <qtdHrsSem>${qtdHrsSem}</qtdHrsSem>
+                    <tpJornada>${tpJornada}</tpJornada>
+                    <tmpParc>${tmpParc}</tmpParc>
+                    <horNoturno>${horNoturno}</horNoturno>
+                    <dscJorn>${dscJorn}</dscJorn>
+                </horContratual>
+                <alvaraJudicial>
+                    <nrProcJud>${nrProcJud}</nrProcJud>
+                </alvaraJudicial>
+                <observacoes>
+                    <observacao>${observacao}</observacao>
+                </observacoes>
+                <treiCap>
+                    <codTreiCap>${codTreiCap}</codTreiCap>
+                </treiCap>
+            </infoContrato>
+            <sucessaoVinc>
+                <tpInsc></tpInsc>
+                <nrInsc></nrInsc>
+                <matricAnt></matricAnt>
+                <dtTransf></dtTransf>
+                <observacao></observacao>
+            </sucessaoVinc>
+            <transfDom><!-- Informações do empregado doméstico transferido--></transfDom>
+            <mudancaCPF><!-- Em caso de mudança do CPF do trabalhador --></mudancaCPF>
+            <afastamento>
+                <dtIniAfast>${dtIniAfast}</dtIniAfast>
+                <codMotAfast>${codMotAfast}</codMotAfast>
+            </afastamento>
+            <desligamento>
+                <dtDeslig>${dtDeslig}</dtDeslig>
+            </desligamento>
+            <cessao> <!-- Preencher apenas em caso de cessão de trabalhador -->
+                <dtIniCessao>${dtIniCessao}</dtIniCessao>
+            </cessao>
+        </vinculo>
+    </evtAdmissao>
+</eSocial>
+`;
+		return msg;
+	}
 </script>
 
 <div id="container">
