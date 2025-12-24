fc6131094c52e4893bcdb489a14d1e6210feed20
Leandro Braga <leandrobraga@Mac-mini-de-Leandro.local>
2025-12-24 17:39:58 -0300

    succesfully implemented XML generation

--- src/routes/gerador/xmlAssembler/+layout.svelte
+++ src/routes/gerador/xmlAssembler/+layout.svelte
@@ -1,40 +1,46 @@
 <script>
-    let msg = `
-    <eSocial>
-    </eSocial>
-    `
 
-    let cpfTrab = ''; 
-    let nmTrab = ''; 
-    let sexo = ''; 
+    let cpfTrab = $state(''); 
+    let nmTrab = $state(''); 
+    let sexo = $state(''); 
     let racaCor = ''; 
     let estCiv = ''; 
     let grauInstr = ''; 
     let nmSoc = ''; 
 
-    let grupo = {
+    let grupo = $derived({
         'trabalhador': `
         <trabalhador>
-            <cpfTrab></cpfTrab>
-            <nmTrab></nmTrab>
-            <sexo></sexo>
-            <racaCor></racaCor>
-            <estCiv></estCiv>
-            <grauInstr></grauInstr>
+            <cpfTrab>${cpfTrab}</cpfTrab>
+            <nmTrab>${nmTrab}</nmTrab>
+            <sexo>${sexo}</sexo>
+            <racaCor>${racaCor}</racaCor>
+            <estCiv>${estCiv}</estCiv>
+            <grauInstr>${grauInstr}</grauInstr>
             <nmSoc></nmSoc>
         </trabalhador>
         `
+    })
+
+    let msg = $derived(`
+    <eSocial>
+        ${grupo.trabalhador}
+    </eSocial>
+    `)
+
+    function gerarXML(){
+        console.log(msg); 
     }
 </script>
 
 <div class="container">
-    <input class="input" placeholder="CPF"/>
-    <input class="input" placeholder="Nome do Trabalhador"/>
-    <select class="select">
+    <input class="input" placeholder="CPF" bind:value={cpfTrab}/>
+    <input class="input" placeholder="Nome do Trabalhador" bind:value={nmTrab}/>
+    <select class="select" bind:value={sexo}>
         <option value="M">M</option>
         <option value="F">F</option>
     </select>
-    <button class="btn">Gerar XML</button>
+    <button class="btn" onclick={gerarXML}>Gerar XML</button>
 </div>
 
 <style>
