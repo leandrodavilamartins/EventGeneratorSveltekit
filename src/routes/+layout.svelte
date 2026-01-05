d5951c581d7731700b01b704f90fc20fa0498e06
Leandro Braga <leandrobraga@Mac-mini-de-Leandro.local>
2026-01-05 13:09:12 -0300

    authentication succesfully implemented

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,74 @@
 <script>
 	import './layout.css';
 	import favicon from '$lib/assets/favicon.svg';
+	import { getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth"; 
+	import { Tabs } from '@skeletonlabs/skeleton-svelte'; 
+
+	const auth = getAuth(); 
+	let email = $state(''); 
+	let password = $state(''); 
+	let loginForm = $state(true); 
+	let isAuthenticated = $state(false);  
+
+	async function authenticate() {
+
+		signInWithEmailAndPassword(auth, email, password)
+		.then((userCredential) => {
+			const user = userCredential.user; 
+			isAuthenticated = true; 
+			loginForm = false; 
+		})
+		.catch((error) => {
+			const errorCode = error.code; 
+			const errorMessage = error.message; 
+		}); 
+	}
+
+	async function deslogar() {
+		signOut(auth).then(() => {
+			console.log("Logout"); 
+			isAuthenticated = false; 
+			loginForm = true; 
+		}).catch((error) => {
+			console.log("Ocorreu um erro"); 
+		}); 
+		
+	}
 
 	let { children } = $props();
 </script>
 
 <svelte:head><link rel="icon" href={favicon} /></svelte:head>
 <div>MENU</div>
+{#if loginForm}
+<div>
+	<form>
+		<fieldset>
+			<label class="label">
+				<span class="label-text">E-mail</span>
+				<input class="input" type="text" bind:value={email}/>
+			</label>
+		</fieldset>
+		<fieldset>
+			<label class="label">
+				<span class="label-text">Senha</span>
+				<input class="input" type="text" bind:value={password}/>
+			</label>
+		</fieldset>
+		<button type="button" class="btn preset-filled" onclick={authenticate}>Login</button>
+	</form>
+</div>
+{/if}
+
+{#if isAuthenticated}
+<Tabs>
+    <Tabs.List>
+        <Tabs.Trigger value="Evento"><a href="/gerador/xmlAssembler">Emitir Evento</a></Tabs.Trigger>
+        <Tabs.Trigger value="Empregador">Cadastrar Empregador</Tabs.Trigger>
+        <Tabs.Trigger value="Trabalhador"><a href="/gerador/cadastro">Cadastrar Trabalhador</a></Tabs.Trigger>
+		<button type="button" class="btn preset-filled" onclick={deslogar}>Sair</button>
+    </Tabs.List>
+</Tabs>
+
 {@render children()}
+{/if}
--- src/routes/gerador/+layout.svelte
+++ src/routes/gerador/+layout.svelte
@@ -1,13 +1,6 @@
 <script>
-    import {Tabs} from '@skeletonlabs/skeleton-svelte'; 
+    
 </script>
 
-<Tabs>
-    <Tabs.List>
-        <Tabs.Trigger value="Evento"><a href="/gerador/xmlAssembler">Emitir Evento</a></Tabs.Trigger>
-        <Tabs.Trigger value="Empregador">Cadastrar Empregador</Tabs.Trigger>
-        <Tabs.Trigger value="Trabalhador"><a href="/gerador/cadastro">Cadastrar Trabalhador</a></Tabs.Trigger>
-    </Tabs.List>
-</Tabs>
 
 <slot></slot>
