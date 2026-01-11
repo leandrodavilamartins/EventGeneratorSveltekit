dd42f4a5f369e5a13950e933155e5c36bef4f027
Leandro Braga <leandrobraga@Mac-mini-de-Leandro.local>
2026-01-11 12:46:35 -0300

    env variables implemented

--- src/lib/firebaseApp.js
+++ src/lib/firebaseApp.js
@@ -1,14 +1,15 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore"
+import { PUBLIC_APIKEY, PUBLIC_AUTHDOMAIN, PUBLIC_PROJECTID, PUBLIC_STORAGEBUCKET, PUBLIC_MESSAGINGSENDERID, PUBLIC_APPID, PUBLIC_MEASUREMENTID } from "$env/static/public";  
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBmP9w2tndSzzZqyMnrIf1qm0EkvGPPdgA",
-  authDomain: "esocialapp-7f98f.firebaseapp.com",
-  projectId: "esocialapp-7f98f",
-  storageBucket: "esocialapp-7f98f.firebasestorage.app",
-  messagingSenderId: "663612866996",
-  appId: "1:663612866996:web:839e5a07d624c083bc32e1",
-  measurementId: "G-E18ZYDTCSX"
+  apiKey: PUBLIC_APIKEY,
+  authDomain: PUBLIC_AUTHDOMAIN,
+  projectId: PUBLIC_PROJECTID,
+  storageBucket: PUBLIC_STORAGEBUCKET,
+  messagingSenderId: PUBLIC_MESSAGINGSENDERID,
+  appId: PUBLIC_APPID,
+  measurementId: PUBLIC_MEASUREMENTID
 };
 
 const app = initializeApp(firebaseConfig); 
